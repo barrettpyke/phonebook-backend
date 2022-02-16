19960f32e2cdcf5d4e01562b8c384646b421c811
Barrett Pyke <barrettpyke@Barretts-MacBook-Pro.local>
2022-02-16 10:25:09 -0600

    3.15 -3.18

--- index.js
+++ index.js
@@ -18,43 +18,79 @@ app.use(morgan(function (tokens, req, res) {
   ].join(' ')
 }))
 app.use(cors());
-app.use(express.static('build'))
+app.use(express.static('build'));
+
+const errorHandler = (error, req, resp, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return resp.status(400).send({ error: 'Malformatted ID' })
+  }
+
+  next(error)  
+}
+
+const unknownEndpoint = (req, resp) => {
+  resp.status(404).send({ error: 'Unknown Endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 //GET DATA
-app.get('/info', (req, resp) => {
-  let timeStamp = Date();
-  resp.send(
+app.get('/info', (req, resp, error) => {
+  Person.find({})
+  .then(persons => {
+    if (persons) {
+    let timeStamp = Date()
     `<p>Phonebook has info for ${persons.length} people.
      <p>${timeStamp}</p>`
-  );
+  } else {
+    resp.status(404).end()
+  }
+  })
+  .catch(error => next(error))
 })
 
-app.get('/api/persons', (req, resp) => {
-  Person.find({}).then(persons => {
-    resp.json(persons)
+
+app.get('/api/persons', (req, resp, next) => {
+  Person.find({})
+  .then(persons => {
+    if (persons) {
+      resp.json(persons)
+    } else {
+      resp.status(404).end()
+    }
   })
+  .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (req, resp) => {
-  Person.findById(req.params.id).then(person => {
-    resp.json(person)
+app.get('/api/persons/:id', (req, resp, next) => {
+  Person.findById(req.params.id)
+  .then(person => {
+    if (person) {
+      resp.json(person)
+    } else {
+      resp.status(404).end()
+    }
   })
+  .catch(error => next(error))
 })
 
 //DELETE PERSON
-app.delete('/api/persons/:id', (req, resp) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  resp.status(204).end()
+app.delete('/api/persons/:id', (req, resp, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      resp.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 //ADD PERSON
-app.post('/api/persons', (req, resp) => {
+app.post('/api/persons', (req, resp, next) => {
   const body = req.body
 
   if (body.name === undefined || body.number === undefined) {
-    return resp.status(400).json({ error: 'Content missing'})
+    resp.status(400).end()
   }
 
   const person = new Person({
@@ -65,7 +101,26 @@ app.post('/api/persons', (req, resp) => {
   person.save().then(savedPerson => {
     resp.json(savedPerson)
   })
+  .catch(error => next(error))
 })
+
+//UPDATE PERSON
+app.put('/api/persons/:id', (req, resp, next) => {
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+  .then(updatedNote => {
+    resp.json(updatedNote)
+  })
+  .catch(error => next(error))
+})
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
